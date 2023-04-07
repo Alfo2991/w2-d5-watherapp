6b144394b63a9e836a75b73d59d070bfacebb87d
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-04-07 19:34:13 +0200

    working

--- src/Home.js
+++ src/Home.js
@@ -8,56 +8,62 @@ export default function Home() {
     name: "London",
     humidity: 18,
     speed: 2,
+    image: ''
   });
+  const [name, setName] = useState("");
 
-  useEffect(() => {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=5486b01d6d00e2240f7508e3402b1f71&units=metric`;
-    axios
-      .get(apiUrl)
-      .then((res) =>
-        setData({
-          ...data,
-          celcius: res.data.main.temp,
-          name: res.data.name,
-          humidity: res.data.main.humidity,
-          speed: res.data.wind.speed,
-        })
-      )
-      .catch((err) => console.log(err));
-  }, []);
-
+  const handleClick = () => {
+    if (name !== "") {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=5486b01d6d00e2240f7508e3402b1f71&units=metric`;
+      axios.get(apiUrl)
+        .then(res => {
+            let imagePath= '';
+       
+          setData({ ...data,  celcius: res.data.main.temp,  name: res.data.name,   humidity: res.data.main.humidity, speed: res.data.wind.speed})
+    })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <div className="container">
       <div className="weather">
         <div className="search">
-          <input type="text" placeholder="Enter City Name" />
+          <input
+            type="text"
+            placeholder="Enter City Name"
+            onChange={(e) => setName(e.target.value)}
+          />
           <button>
             {" "}
-            <img src="/images/search.png" alt="search" />{" "}
+            <img
+              src="/images/search.png"
+              alt="search"
+              onClick={handleClick}
+            />{" "}
           </button>
         </div>
         <div className="weatherInfo">
           <img src="/images/mist.png" alt="" />
-          <h1>{data.celsius}°C</h1>
+          <h1>{Math.round(data.celsius)}°C</h1>
           <h2>{data.name}</h2>
           <div className="details">
             <div className="col">
               <img src="/images/humidity.png" alt="" />
               <div className="humidity">
-                <p>{data.humidity}%</p>
+                <p>{Math.round(data.humidity)}%</p>
                 <p>Humidity</p>
               </div>
             </div>
             <div className="col">
-            <img src="/images/wind.png" alt="" />
-            <div className="wind">
-              <p>2 Km/h</p>
-              <p>{data.wind}</p>
+              <img src="/images/wind.png" alt="" />
+              <div className="wind">
+                <p>{Math.round(data.speed)}Km/h</p>
+                <p>Wind</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-      </div>
-      );
+    </div>
+  );
 }
